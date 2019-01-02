7eb71c41083eea1ad7b146ed5d572815acfc5528
George Kefalas <44002403+gkef1996@users.noreply.github.com>
2019-01-03 01:47:02 +0200

    Add files via upload

--- back-end/RestAPI/src/main/java/gr/ntua/ece/softeng18b/api/RestfulApp.java
+++ back-end/RestAPI/src/main/java/gr/ntua/ece/softeng18b/api/RestfulApp.java
@@ -17,14 +17,17 @@ public class RestfulApp extends Application {
         //GET, PATCH, PUT, DELETE
         router.attach("/products/{id}", ProductResource.class);
         
-       //GET, POST
-       router.attach("/shops", ShopsResource.class);
+        //GET, PATCH, PUT, DELETE
+        router.attach("/productswithimage/{id}", ProductWithImageResource.class);
         
-     //GET, PATCH, PUT, DELETE
-       router.attach("/shops/{id}", ShopResource.class);
+        //GET, POST
+        router.attach("/shops", ShopsResource.class);
+        
+        //GET, PATCH, PUT, DELETE
+        router.attach("/shops/{id}", ShopResource.class);
        
-     //GET, POST
-       router.attach("/prices", PricesResource.class);
+        //GET, POST
+        router.attach("/prices", PricesResource.class);
 
         return router;
     }
