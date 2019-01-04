64e1e368e9f69baeef382af8cc5b2a8bb7168c21
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-01-04 21:17:47 +0200

    Added image from binary. img hover

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
     console.log("ready");
 
-    var input = 1;
+    var productID = 2;
     $.ajax({
         type: "GET",
         dataType: "json",
-        url: "http://localhost:8765/app/observatory/api/products/"+input,
+        url: "http://localhost:8765/app/observatory/api/productswithimage/"+productID,
         success: function(data){
             console.log(data);
             var obj = JSON.parse(JSON.stringify(data));
@@ -17,9 +17,16 @@ $(document).ready(function(){
             $.each(tags, function(key,value){
                 $("#tags").append("<li class=\"list-inline-item\">"+value+"</li>");
             });
+           
+            // Convert binaryData to image
+            let binary = new Uint8Array(obj.image.binaryData);
+            let blob = new Blob([binary]);
+            let img = new Image();
+
+            $("#productImage").attr("src",URL.createObjectURL(blob));
         },
         error: function(){
-            console.log("Product.js : Error product with id " + input + " not found !");
+            console.log("Product.js : Error product with id " + productID + " not found !");
         }
     });
 
