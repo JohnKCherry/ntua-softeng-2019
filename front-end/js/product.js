63fd93a47422b37e02b595d46396235d5c60596c
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-02-25 22:29:32 +0200

    Shops not found msg

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -158,6 +158,7 @@ $(document).ready(function(){
     function shopsNotFound() {
                 console.log("Product.js :Prices GET Error product with id " + productID+ " not found !");
             //    $("#shopsDiv").text("Shops Not Found");
+                $("#shops").append("<div class=\"h3\"> Shops Not Found. Use other Filters");
                 $("#map").text("Error Map");
                 //$("#shopsDiv").load("404.html");
                 //$("html").load("404.html");
