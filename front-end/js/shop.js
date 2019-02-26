bf24ec9ea0ece327ce63b63c25c9133d37d39a77
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-02-26 02:35:55 +0200

    Set visibly/hidden buttons

--- front-end/js/shop.js
+++ front-end/js/shop.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
     $.ajax({
         type: "GET",
         dataType: "json",
-        url: "http://localhost:8765/observatory/api/shops/"+shopID,
+        url: "https://localhost:8765/observatory/api/shops/"+shopID,
         success: function(data){
             console.log(data);
             var obj = JSON.parse(JSON.stringify(data));
@@ -70,7 +70,7 @@ $(document).ready(function(){
             type: "GET",
             async: false,
             dataType: "json",
-            url: "http://localhost:8765/observatory/api/shops/"+id,
+            url: "https://localhost:8765/observatory/api/shops/"+id,
             success: function(data){
                 var obj = JSON.parse(JSON.stringify(data));
                 shopArray = [obj.name,obj.lat,obj.lng];
@@ -106,9 +106,9 @@ $(document).ready(function(){
         map = L.map('map').setView([37.592724,23.441932], 8);
 
         mapLink = 
-            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+            '<a href="https://openstreetmap.org">OpenStreetMap</a>';
         L.tileLayer(
-            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; ' + mapLink + ' Contributors',
             maxZoom: 18,
         }).addTo(map);
@@ -118,6 +118,9 @@ $(document).ready(function(){
         .addTo(map);
     }
 	
+	$("#cancelButton").click(function(){
+        console.log("Ela");
+	});
 	$("#editButton").click(function(){
 		$("#shopWebsite").replaceWith($('<p id=\"shopWebsite\" class="h3">Website: <input type=\"text\" id=\"shopWebsite2\" name=\"shopWebsite2\" class="h4" value="' + website + '"><br></input></p>'));
 		$("#shopTel").replaceWith($('<p id=\"shopTel\" class="h4">Telephone: <input type=\"text\" id=\"shopTel2\" name=\"shopTel2\" class="h4" value="' + telephone + '"><br></input></p>'));
@@ -125,16 +128,10 @@ $(document).ready(function(){
 		$("#shopName").replaceWith($('<p id=\"shopName\" class="h2">Name: <input type=\"text\" id=\"shopName2\" name=\"shopName2\" class="h4" value="' + name + '"><br></input></p>'));
 		$("#shopType").replaceWith($('<p id=\"shopType\" class="h4">Categories: <input type=\"text\" id=\"shopType2\" name=\"shopType2\" class="h4" value="' + type + '"><br></input></p>'));
 		$("#tagsHeader").replaceWith($('<p id=\"tags\" class="h4">Tags: <input type=\"text\" id=\"tags2\" name=\"tags2\" class="h4" value="' + tags + '"><br></input></p>'));
-		$("#editButton").replaceWith($('<button id=\"applyButton\" type=\"button\" class=\"btn btn-outline-success\">Apply</button>'));
-		$("#cancelButton").replaceWith($('<button id=\"cancelButton\" type=\"button\" class=\"btn btn-outline-danger\">Cancel</button>'));
-		
-		
-	});
-	$("#cancelButton").click(function(){
-		alert("wtf");
-	
-		
-		
+
+        $("#editButton").css("visibility","hidden");
+        $("#applyButton").css("visibility","visible");
+        $("#cancelButton").css("visibility","visible");
 		
 	});
 	
