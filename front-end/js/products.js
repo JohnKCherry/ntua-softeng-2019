633b5fb4c5168d662c48475c72cdc99f6d4002b8
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-02-27 19:37:26 +0200

    Collapse description, added filters

--- front-end/js/products.js
+++ front-end/js/products.js
@@ -28,6 +28,9 @@ $(document).ready(function(){
         $("#loginBtn").show();
     }
 
+
+
+
     var id;
     var name;
     var description;
@@ -40,18 +43,26 @@ $(document).ready(function(){
     var count = 12;
     var sort = "id";
     var order = 1;
+    $("#order").val("1");
     var orderStr = "ASC";
-    var status = "ALL";
+    var status = 1;
+    $("#status").val("1");
+    var statusStr = "ALL";
 
-    function getProducts(start,count,sort,order) {
+
+    function getProducts(start,count,sort,order,status) {
         // get product general info
 
+        $(".card-deck").empty();
         orderStr = (order==1) ? "ASC" : "DESC";
+        if (status == 1) statusStr = "ALL";
+        else if (status == 2) statusStr = "ACTIVE";
+        else if (status == 3) statusStr = "WITHDRAWN";
         var url = "https://localhost:8765/observatory/api/productswithimage?start="+start
         +"&count="+count
         +"&sort="+sort
         +"|"+orderStr
-        +"&status="+status;
+        +"&status="+statusStr;
 
         console.log(url);
         $.ajax({
@@ -64,7 +75,6 @@ $(document).ready(function(){
                 var products = obj.products;
 
                 $.each(products, function(key,value){
-                    console.log(value);
                     id = value.id;
                     name = value.name;
                     description = value.description;
@@ -80,20 +90,30 @@ $(document).ready(function(){
                     }
                     else imgSrc="";
                     // create html
-                    $(".card-deck").append("<div class=\"col-sm-6 col-md-4 col-lg-3\"><div class=\"card mb-4\"><img class=\"card-img-top img-fluid\" src=\""+imgSrc+"\" alt=\"Product Image\"><div class=\"card-body\"><a href=\"product.html?id="+id+"\" class=\"card-title\">"+name+"</a><p class=\"card-text\">"+description+"</p></div><div class=\"card-footer\"><small class=\"text-muted\">"+category+"</small></div></div></div></div>"
-                    );
+                    $(".card-deck").append("<div class=\"col-sm-6 col-md-4 col-lg-3\"><div class=\"card mb-4\"><img class=\"card-img-top img-fluid\" src=\""+imgSrc+"\" alt=\"Product Image\"><div class=\"card-body\"><a href=\"product.html?id="+id+"\" class=\"card-title\">"+name+"</a><br /><a class=\"text-secondary collapsed card-link\" data-toggle=\"collapse\" href=\"#collapse"+id+"\">Read Description</a><div id=\"collapse"+id+"\" class=\"collapse\"><p class=\"card-text\">"+description+"</p></div></div><div class=\"card-footer\"><small class=\"text-muted\">"+category+"</small></div></div></div></div>"
+                                          );
                 });
             },
             error: function(){
                 console.log("Products.js : Error get products !!");
                 $("body").load("404.html");
                 return false;
-                // window.location.href = "404.html?error=1";
             }
         });
     }
 
-    getProducts(start,count,sort,order);
+    getProducts(start,count,sort,order,statusStr);
 
+    // event listener order
+    // order change reload products
+    $("#order").change(function() {
+        order = $("#order").val();
+        getProducts(0,12,sort,order,status);
+    });
+    
+    $("#order").change(function() {
+        order = $("#order").val();
+        getProducts(0,12,sort,order,status);
+    });
 });
 
