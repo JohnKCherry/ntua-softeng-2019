7f68c7b18112706d33cae2a888fa3d3ad63ffd27
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-02-28 01:03:41 +0200

    Added favourite icon to product.html

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -41,10 +41,13 @@ $(document).ready(function(){
         $("#loginBtn").text(window.sessionStorage.getItem("username"));
         $("#loginBtn").attr("href","");
         $("#editButton").css("visibility","visible");
+        $("#fav").css("visibility","visible");
     }
     else {
         $("#loginBtn").show();
     }
+
+
     var productID = getUrlParameter('id');
     if (productID == null) productID = 27;
     var lowestPrice;
@@ -86,6 +89,7 @@ $(document).ready(function(){
             else {
                 $("#productImage").attr("src","imgs/product.jpg");
             }
+            getFav();
         },
         error: function(){
             console.log("Product.js : Error product with id " + productID + " not found !");
@@ -94,6 +98,37 @@ $(document).ready(function(){
             // window.location.href = "404.html?error=1";
         }
     });
+
+
+    // get favourites req
+    function getFav() {
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            headers: {'X-OBSERVATORY-AUTH' : token},
+            url: "https://localhost:8765/observatory/api/favourites",
+            success: function(data){
+                console.log(data);
+                var obj = JSON.parse(JSON.stringify(data));
+                var products = obj.products;
+
+                $.each(products, function(key,value) {
+                    if(value.id == productID) {
+                        $("#fav").attr('class','fas fa-heart');
+                        $("#fav").attr('data-val','1');
+                        $("#fav").attr('title','Remove from favourites');
+                        return false;
+                    }
+                });
+            },
+            error: function(){
+                console.log("Product.js : Error get favourites !");
+                return false;
+                // window.location.href = "404.html?error=1";
+            }
+
+        });
+    }
 
     // get prices and shops
     function shopsUpdate(reload){
@@ -114,7 +149,7 @@ $(document).ready(function(){
         if ( sort == 1 ) sortStr = "price";
         else if ( sort == 2 ) sortStr = "date";
         else sortStr = "dist";
-        
+
         // an epelekses apostasi findlocation
         if(sortStr == "dist") {
             findLocation();
@@ -189,7 +224,7 @@ $(document).ready(function(){
             error: function(){
                 console.log("Product.js :Prices GET Error product with id " + productID + " not found !");
                 // $("#shopsDiv").text("Shops Not Found");
-              //  $("#map").text("Error Map");
+                //  $("#map").text("Error Map");
                 //$("#shopsDiv").load("404.html");
                 //$("html").load("404.html");
                 return ;
@@ -204,7 +239,7 @@ $(document).ready(function(){
         //    $("#shopsDiv").text("Shops Not Found");
         $("#shops").append("<div class=\"h3\"> Shops Not Found. Use other Filters");
         markersLayer.clearLayers();
-       // $("#map").text("Error Map");
+        // $("#map").text("Error Map");
         //$("#shopsDiv").load("404.html");
         //$("html").load("404.html");
         return ;
@@ -229,7 +264,7 @@ $(document).ready(function(){
             error: function(){
                 console.log("Product.js :Shop with id " + id + " not found !");
                 $("#shopsDiv").text("Shops Not Found");
-        //        $("#map").text("Error Map");
+                //        $("#map").text("Error Map");
                 return ;
             }
         });
@@ -265,8 +300,8 @@ $(document).ready(function(){
             map.off();
             map.remove();
         }
-//        if( gps[0] == "") map = L.map('map').setView([37.592724,23.441932], 8);
-  //      else map = L.map('map').setView([gps[0],gps[1]], 12);
+        //        if( gps[0] == "") map = L.map('map').setView([37.592724,23.441932], 8);
+        //      else map = L.map('map').setView([gps[0],gps[1]], 12);
 
 
         // to pio panw magazi
@@ -286,7 +321,7 @@ $(document).ready(function(){
         for (var i = 0; i < locations.length; i++) {
             marker = new L.marker([locations[i][1],locations[i][2]])
             .bindPopup(locations[i][0]);
-           // .addTo(map);
+            // .addTo(map);
             markersLayer.addLayer(marker); 
         }
 
@@ -427,6 +462,67 @@ $(document).ready(function(){
         }
 
     };
+
+    $("#fav").on('click',function(){
+        console.log("Heart clicked");
+        var val = $("#fav").attr('data-val');
+        if (val == 0) {
+            console.log("To agapaw");
+            addToFav();
+        }
+        else {
+            console.log("To misw");
+            removeFromFav();
+        }
+    });
+    function addToFav() {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            headers: {'X-OBSERVATORY-AUTH' : token},
+            url: "https://localhost:8765/observatory/api/favourites",
+            data:{"productId":productID},
+            success: function(data){
+                console.log("Success add to favourites");
+                var obj = JSON.parse(JSON.stringify(data));
+                console.log(obj);
+                $("#fav").attr('class','fas fa-heart');
+                $("#fav").attr('data-val','1');
+                $("#fav").attr('title','Remove from favourites');
+            },
+            error: function(err){
+                console.log("product.js: Error add to favourites");
+                console.log(err);
+                $("#fav").attr('class','far fa-heart');
+                $("#fav").attr('data-val','0');
+                $("#fav").attr('title','Add to favourites');
+            }
+        });
+    }
+
+    function removeFromFav() {
+        $.ajax({
+            type: "DELETE",
+            dataType: "json",
+            headers: {'X-OBSERVATORY-AUTH' : token},
+            url: "https://localhost:8765/observatory/api/favourites/"+productID,
+            success: function(data){
+                console.log("Success remove from favourites");
+                var obj = JSON.parse(JSON.stringify(data));
+                console.log(obj);
+                $("#fav").attr('class','far fa-heart');
+                $("#fav").attr('data-val','0');
+                $("#fav").attr('title','Add to favourites');
+            },
+            error: function(err){
+                console.log("product.js: Error remove from favourites");
+                console.log(err);
+                $("#fav").attr('class','fas fa-heart');
+                $("#fav").attr('data-val','1');
+                $("#fav").attr('title','Remove from favourites');
+            }
+        });
+    }
 
 
     function sendUpdate(obj) {
