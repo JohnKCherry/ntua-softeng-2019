f93edf2f896aee10af080c91d52f634f4aff5639
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-02-27 21:07:17 +0200

    Added shops.html, need to add map for images

--- front-end/js/shops.js
+++ front-end/js/shops.js
@@ -33,11 +33,8 @@ $(document).ready(function(){
 
     var id;
     var name;
-    var description;
-    var category;
+    var address;
     var tags;
-    var image;
-    var imgSrc;
 
     var start = 0;
     var count = 12;
@@ -50,15 +47,14 @@ $(document).ready(function(){
     var statusStr = "ALL";
 
 
-    function getProducts(start,count,sort,order,status,clear) {
-        // get product general info
+    function getShops(start,count,sort,order,status,clear) {
+        // get shops general info
 
         if(clear) $(".card-deck").empty();
         orderStr = (order==1) ? "ASC" : "DESC";
         if (status == 1) statusStr = "ALL";
         else if (status == 2) statusStr = "ACTIVE";
-        else if (status == 3) statusStr = "WITHDRAWN";
-        var url = "https://localhost:8765/observatory/api/productswithimage?start="+start
+        var url = "https://localhost:8765/observatory/api/shops?start="+start
         +"&count="+count
         +"&sort="+sort
         +"|"+orderStr
@@ -72,52 +68,38 @@ $(document).ready(function(){
             success: function(data){
                 var obj = JSON.parse(JSON.stringify(data));
                 console.log(obj);
-                var products = obj.products;
-
-                $.each(products, function(key,value){
+                var shops = obj.shops;
+                $.each(shops, function(key,value){
                     id = value.id;
                     name = value.name;
-                    description = value.description;
-                    category = value.category;
-                    image = value.image;
-                    // create image
-                    if(image != null) {
-                        // Convert binaryData to image
-                        let binary = new Uint8Array(image.binaryData);
-                        let blob = new Blob([binary]);
-                        let img = new Image();
-                        imgSrc = URL.createObjectURL(blob);
-                    }
-                    else imgSrc="";
+                    address = value.address;
                     // create html
-                    $(".card-deck").append("<div class=\"col-sm-6 col-md-4 col-lg-3\"><div class=\"card mb-4\"><img class=\"card-img-top img-fluid\" src=\""+imgSrc+"\" alt=\"Product Image\"><div class=\"card-body\"><a href=\"product.html?id="+id+"\" class=\"card-title\">"+name+"</a><br /><a class=\"text-secondary collapsed card-link\" data-toggle=\"collapse\" href=\"#collapse"+id+"\">Read Description</a><div id=\"collapse"+id+"\" class=\"collapse\"><p class=\"card-text\">"+description+"</p></div></div><div class=\"card-footer\"><small class=\"text-muted\">"+category+"</small></div></div></div></div>"
-                                          );
+                    $(".card-deck").append("<div class=\"col-sm-6 col-md-4 col-lg-3\"><div class=\"card mb-4\"><div class=\"card-body\"><a href=\"product.html?id="+id+"\" class=\"card-title\">"+name+"</a><p class=\"card-text\">"+address+"</p></div></div></div>");
                 });
             },
             error: function(){
-                console.log("Products.js : Error get products !!");
+                console.log("Shops.js : Error get shops !!");
                 $("body").load("404.html");
                 return false;
             }
         });
     }
 
-    getProducts(start,count,sort,order,statusStr,1);
+    getShops(start,count,sort,order,statusStr,1);
 
     // event listener order
     // order change reload products
     $("#order").change(function() {
         order = $("#order").val();
-        getProducts(0,12,sort,order,status,1);
+        getShops(0,12,sort,order,status,1);
     });
 
     // status event listener
     $("#status").change(function() {
         status = $("#status").val();
         console.log("Status changed to " + status);
-       // getProducts(0,12,sort,order,status,1);
+       // getShops(0,12,sort,order,status,1);
     });
-
 
     // function take height for all browsers
     function getDocHeight() {
@@ -134,7 +116,7 @@ $(document).ready(function(){
     $(window).scroll(function() {
         if($(window).scrollTop() + $(window).height() > getDocHeight() - 100) {
             start = start+11;
-            getProducts(start,12,sort,order,status,0);
+            getShops(start,12,sort,order,status,0);
         }
     });
 });
