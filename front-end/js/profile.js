0ace2790a7e190cd84cc82d07f979d3ed5d53e12
anthivaios <anthivaios@yahoo.gr>
2019-03-02 15:04:29 +0200

    profile progress

--- front-end/js/profile.js
+++ front-end/js/profile.js
@@ -1,15 +1,53 @@
 
-        $.ajax({
-            type: "POST",
-            dataType: "json",
-            headers: {'X-OBSERVATORY-AUTH' : '25276e4fd3b907a8a11989dc00df23df9fe0a1ef7a6f1c1e6655dbdb88f00c5adaniel'},
-            url: "http://localhost:8765/observatory/api/profile",
-            success: function(data){
-                console.log("Success");
-                var obj = JSON.parse(JSON.stringify(data));
-                console.log(obj);
-            },
-            error: function(){
-                console.log("Login.js : Error to login");
-            }
+
+
+
+var token = window.sessionStorage.getItem("token");
+$(document).ready(function(){
+        console.log("ready");
+	
+	    console.log("Token ");
+        console.log(token);
+		
+		if (token != null) {
+            $("#loginBtn").text(window.sessionStorage.getItem("username"));
+            $("#loginBtn").attr("href","");
+            $("#editButton").css("visibility","visible");
+        }
+        else {
+             $("#loginBtn").show();
+		}
+		
+		
+		
+		//GET INFO
+		$.ajax({
+        type: "POST",
+        dataType: "json",
+		headers: {'X-OBSERVATORY-AUTH' : token},
+        url: "https://localhost:8765/observatory/api/profile",
+		data: {token},
+        success: function(data){
+            console.log("GOT PROFILE DETAILS");
+            var obj = JSON.parse(JSON.stringify(data));           
+            $("#pName").append("<span class =\"h5\">" + obj.fullname + "</span>");
+            $("#pUsername").append("<span class =\"h5\">" + obj.username + "</span>");
+            $("#pEmail").append("<span class =\"h5\">" + obj.email + "</span>");
+            
+
+        },
+        error: function(){
+            console.log("profile Error");
+            $("body").load("404.html");
+            return false;
+            // window.location.href = "404.html?error=1";
+        }
         });
+		
+	
+
+
+
+
+
+});
