c58566d7ff3f16bfba5e57dfad926f78cdc0f5db
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-03-03 18:42:59 +0200

    ready map, need to add current location

--- front-end/js/prices.js
+++ front-end/js/prices.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
         productTags = query.split(' ').join('+');
         console.log(productTags);
     }
-    
+
     var query2 = getUrlParameter('query2');
     if (query2 == null) $("#shopBar").val("");
     else {
@@ -40,7 +40,7 @@ $(document).ready(function(){
         shopTags = query.split(' ').join('+');
         console.log(shopTags);
     }
-    
+
     var map = null;
     var productName;
     var productID;
@@ -62,7 +62,7 @@ $(document).ready(function(){
 
     var start = 0;
     var count = "";
-    
+
     // get prices and shops
     function getPrices(reload){
 
@@ -72,7 +72,7 @@ $(document).ready(function(){
 
         if ($("#productBar").val() == "") productTags = "";
         if ($("#shopBar").val() == "") shopTags = "";
-       
+
         sort = $("#sort").val();
         order = $("#order").val();
         var geoDist  = "";
@@ -119,7 +119,7 @@ $(document).ready(function(){
             $("#errorFilters").text("Date From must be earlier than Date To"); 
             shopsNotFound();
         }
-        
+
         var url = "https://localhost:8765/observatory/api/prices?verbose=false&start="+start
         +"&count="+count
         +"&geoDist="+geoDist
@@ -135,7 +135,7 @@ $(document).ready(function(){
         +"&sort="+sortStr
         +"|"+orderStr;
         console.log(url); 
-       // var shopsID = new Array();
+        // var shopsID = new Array();
         $.ajax({
             type: "GET",
             dataType: "json",
@@ -161,10 +161,10 @@ $(document).ready(function(){
                         var hash = shopName.split(' ').join('');
                         hash += productID;
                         getProduct(productID);        
-                        $(".card-deck").append("<div class=\"col-sm-6 col-md-4 col-lg-3\"><div class=\"card mb-4\"><img class=\"card-img-top img-fluid\" src=\""+productImage+"\" alt=\"Product Image\"><div class=\"card-body\"><a href=\"product.html?id="+productID+"\" class=\"card-title\">"+productName+"</a><br /><a class=\"text-secondary collapsed card-link\" data-toggle=\"collapse\" href=\"#"+hash+"\">Price</a><div id=\""+hash+"\" class=\"\" aria-expanded=\"true\"><p class=\"card-text\">"+productPrice+"&euro;</p></div></div><div class=\"card-footer\"><a href=\"shop.html?=id="+shopID+"\"<small class=\"text-muted\">"+shopName+"</small></a></div></div></div></div>"
-                                          );
+                        $(".card-deck").append("<div class=\"col-sm-6 col-md-4 col-lg-3\"><div class=\"card mb-4\"><img class=\"card-img-top img-fluid\" src=\""+productImage+"\" alt=\"Product Image\"><div class=\"card-body\"><a href=\"product.html?id="+productID+"\" class=\"card-title\">"+productName+"</a><br /><a class=\"text-secondary collapsed card-link\" data-toggle=\"collapse\" href=\"#"+hash+"\">Price</a><div id=\""+hash+"\" class=\"\" aria-expanded=\"true\"><p class=\"card-text\">"+productPrice+"&euro;</p></div><button id=\"modalBtn\" data-product=\""+productID+"\" data-shop=\""+shopID+"\" type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#mapModal\">Show Map</button></div><div class=\"card-footer\"><a href=\"shop.html?=id="+shopID+"\"<small class=\"text-muted\">"+shopName+"</small></a></div></div></div></div>"
+                                              );
                     });
-                   // if (reload == 1) setMap(shopsID);
+                    // if (reload == 1) setMap(shopsID);
                 }
             },
             error: function(){
@@ -176,32 +176,32 @@ $(document).ready(function(){
     }
 
     function getProduct(currID) {
-        
+
         console.log("prices.js: tha kanw get product gia to " + currID);
-    // get product general info
-    $.ajax({
-        type: "GET",
-        async: false,
-        dataType: "json",
-        url: "https://localhost:8765/observatory/api/productswithimage/"+currID,
-        success: function(data){
-            console.log(data);
-            var obj = JSON.parse(JSON.stringify(data));
-            if(obj.image != null) {
-                let binary = new Uint8Array(obj.image.binaryData);
-                let blob = new Blob([binary]);
-                let img = new Image();
-                productImage = URL.createObjectURL(blob);
+        // get product general info
+        $.ajax({
+            type: "GET",
+            async: false,
+            dataType: "json",
+            url: "https://localhost:8765/observatory/api/productswithimage/"+currID,
+            success: function(data){
+                console.log(data);
+                var obj = JSON.parse(JSON.stringify(data));
+                if(obj.image != null) {
+                    let binary = new Uint8Array(obj.image.binaryData);
+                    let blob = new Blob([binary]);
+                    let img = new Image();
+                    productImage = URL.createObjectURL(blob);
+                }
+                else {
+                    productImage = "imgs/product.jpg";
+                }
+            },
+            error: function(){
+                console.log("Prices.js : Error get product with id " + currID + " not found !");
+                return false;
             }
-            else {
-                productImage = "imgs/product.jpg";
-            }
-        },
-        error: function(){
-            console.log("Prices.js : Error get product with id " + currID + " not found !");
-            return false;
-        }
-    });
+        });
 
     }
     //listener search bar send request
@@ -213,7 +213,7 @@ $(document).ready(function(){
             console.log(productTags);
         }
         getPrices(1);
-           
+
     });
     //listener search bar send request
     $("#shopBar").on("keyup", function() {
@@ -224,7 +224,7 @@ $(document).ready(function(){
             console.log(shopTags);
         }
         getPrices(1);
-           
+
     });
     // event listener order
     // order change reload products
@@ -246,7 +246,7 @@ $(document).ready(function(){
     $("#filters").submit(function() {
         console.log("Form submitted");
         // update shops and reload map
-        
+
         getPrices(1);
 
         return false;   //prevent default
@@ -254,10 +254,24 @@ $(document).ready(function(){
 
 
 
+    $(document).on('click', '#modalBtn',function() {
+        console.log("prices.js: Patithike to modal");
+        var pID = $(this).attr('data-product');
+        var sID = $(this).attr('data-shop');
+        console.log("Product: " + pID + " Shop: " + sID);
+        setMap(sID);
+    });
+
+    $('#mapModal').on('hidden.bs.modal', function () {
+        console.log("Modal eklseise");
+        map.off();
+        map.remove();
+        map = null;
+    })
     // run...
 
     getPrices(1);
-    
+
     // get shop by id
     // input shop id
     // returns obj [name,x,y]
@@ -286,15 +300,8 @@ $(document).ready(function(){
     // input: list of shops ids
     // return: list of locations
     function shopsLocation(shopsID) {
-        var locations = new Array();
-        for(let i=0; i < shopsID.length; i++){
-            locations[i] = new Array();
-            getShopByID(shopsID[i]);
-            locations[i][0] = shopsArray[0];
-            locations[i][1] = shopsArray[1];
-            locations[i][2] = shopsArray[2];
-        }
-        return locations;
+        getShopByID(shopsID);
+        return shopsArray;
     }
 
     // Create Map
@@ -302,22 +309,24 @@ $(document).ready(function(){
     // return: null
 
     function setMap(shopsID) {
+        if ( map != null) {
+            map.off();
+            map.remove();
+        }
         markersLayer = new L.LayerGroup(); // NOTE: Layer is created here!
         console.log("Creating map...");
         var locations = [];
         locations = shopsLocation(shopsID);
         console.log("Get locations..." + locations);
-        // Athens View
-        if ( map != null) {
-            map.off();
-            map.remove();
-        }
-        //        if( gps[0] == "") map = L.map('map').setView([37.592724,23.441932], 8);
-        //      else map = L.map('map').setView([gps[0],gps[1]], 12);
+
+        map = L.map('map', {
+            center: [locations[1], locations[2]],
+            zoom: 12
+        });
 
 
         // to pio panw magazi
-        map = L.map('map').setView([locations[0][1],locations[0][2]], 12);
+        //  map = L.map('map').setView([locations[1],locations[2]], 12);
 
 
 
@@ -329,13 +338,10 @@ $(document).ready(function(){
             maxZoom: 18,
         }).addTo(map);
 
-        console.log("loop");
-        for (var i = 0; i < locations.length; i++) {
-            marker = new L.marker([locations[i][1],locations[i][2]])
-            .bindPopup(locations[i][0]);
-            // .addTo(map);
-            markersLayer.addLayer(marker); 
-        }
+        marker = new L.marker([locations[1],locations[2]])
+        .bindPopup(locations[0]);
+
+        markersLayer.addLayer(marker); 
 
         markersLayer.addTo(map);
     }
@@ -349,7 +355,7 @@ $(document).ready(function(){
             navigator.geolocation.getCurrentPosition(function(position){
                 gps[0] = position.coords.latitude;
                 gps[1] = position.coords.longitude;
-              //  getPrices(1);
+                //  getPrices(1);
             }, function() {
                 console.log("Don't allow location");
                 $("#errorFilters").text("Allow location please"); 
