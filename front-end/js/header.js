110f67e961822476455d3ba31633fb44ad2f1a06
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2019-03-03 22:43:41 +0200

    Hide profile button when you are guest

--- front-end/js/header.js
+++ front-end/js/header.js
@@ -11,15 +11,18 @@ $(document).ready(function () {
         document.getElementById("logout").style.display = "none";
         document.getElementById("fav-button").style.display = "none";
         document.getElementById("add-button").style.display = "none";
+        document.getElementById("username-l").textContent = username;
+        $("#username").hide();
     }
     else {
         username = window.sessionStorage.getItem("username");
         document.getElementById("login").style.display = "none";
+        document.getElementById("username").textContent = username;
+        document.getElementById("username-l").textContent = username;
+        $("#username").show();
+        $("#username").replaceWith("<a href=\"profile.html\" class=\"dropdown-item \" role=\"button\">"+username+"</a>");
     }
-    document.getElementById("username").textContent = username;
-    document.getElementById("username-l").textContent = username;
 
-    $("#username").replaceWith("<a href=\"profile.html\" class=\"dropdown-item \" role=\"button\">"+username+"</a>");
 
 
 });
